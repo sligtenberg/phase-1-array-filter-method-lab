bd9f34448a2436a643f1a4bb3179301a39b6ce9a
stephen ligtenberg <bountyhunter24@gmail.com>
2022-09-29 01:24:37 -0400

    commit tree a7c88c1ee6d1bacc1d765f40a269a9c7684429a4

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+// const findMatching = function (names, driverToMatch) {
+//     return names.filter(function (name) {
+//         return name.toLowerCase() === driverToMatch.toLowerCase();
+//     });
+// }
+
+// rewritten with arrow functions below
+const findMatching = (names, driverToMatch) => names.filter((name) => name.toLowerCase() === driverToMatch.toLowerCase());
+
+const fuzzyMatch = (names, driverToMatchFirstLetter) => names.filter((name) => name.slice(0, 1) === driverToMatchFirstLetter.slice(0, 1));
+
+// original below
+// const matchName = function (driversObj, nameToMatch) {
+//     return driversObj.filter((driver) => driver.name === nameToMatch);
+// }
+
+// modified:
+const matchName = (driversObj, nameToMatch) => driversObj.filter((driver) => driver.name === nameToMatch);
